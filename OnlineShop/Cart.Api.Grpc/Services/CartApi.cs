db87b812cac642f7955451bf5f403532509eb8a5
Maksim Reznikov <maksim_reznikov@epam.com>
2025-01-02 16:37:47 +0200

    Add schell for grpc

--- OnlineShop/Cart.Api.Grpc/Services/CartApi.cs
+++ OnlineShop/Cart.Api.Grpc/Services/CartApi.cs
@@ -1,8 +1,10 @@
-﻿using Grpc.Core;
+﻿using Cart.Core.Abstractions.Inbound;
+using Cart.Core.Entities;
+using Grpc.Core;
 
 namespace Cart.Api.Services;
 
-public class CartApi : Cart.CartBase
+public class CartApi(ICartService cartService) : Cart.CartBase
 {
     public override Task<SimpleResponse> SayHello(SimpleRequest request, ServerCallContext context)
     {
@@ -10,5 +12,59 @@ public class CartApi : Cart.CartBase
         {
             Message = "Hello " + request.Name
         });
+    }
+
+    public override async Task<ItemListResponse> GetList(ItemListRequest request,
+        ServerCallContext context)
+    {
+        var cartId = Guid.Parse(request.CartId);
+        var cart = await cartService.Get(cartId);
+        return cart.ToResponse();
+    }
+
+    public override async Task GetListStream(ItemListRequest request, IServerStreamWriter<ItemResponse> responseStream,
+        ServerCallContext context)
+    {
+    }
+
+    public override async Task<ItemListResponse> AddItem(AddItemRequest request, ServerCallContext context)
+    {
+        var cartId = Guid.Parse(request.CartId);
+        var itemDto = request.Data.ToItemEntity();
+        var cartEntity = await cartService.AddItem(cartId, itemDto);
+        return cartEntity.ToResponse();
+    }
+
+    public override async Task<ItemListResponse> AddItemClientStream(IAsyncStreamReader<AddItemRequest> requestStream,
+        ServerCallContext context)
+    {
+        ItemListResponse? response = null;
+        await foreach (var request in requestStream.ReadAllAsync(context.CancellationToken))
+        {
+            await AddItem(request, context);
+        }
+
+        return response!;
+    }
+
+    public override async Task AddItemBidirectional(IAsyncStreamReader<AddItemRequest> requestStream,
+        IServerStreamWriter<ItemResponse> responseStream,
+        ServerCallContext context)
+    {
+        await foreach (var request in requestStream.ReadAllAsync(context.CancellationToken))
+        {
+            var cartId = Guid.Parse(request.CartId);
+            var itemDto = request.Data.ToItemEntity();
+            await cartService.AddItem(cartId, itemDto);
+            await responseStream.WriteAsync(itemDto.ToResponse(), context.CancellationToken);
+        }
+    }
+
+    public override async Task<ItemListResponse> RemoveItem(RemoveItemRequest request, ServerCallContext context)
+    {
+        var cartId = Guid.Parse(request.CartId);
+        var itemId = request.ItemId;
+        var cartEntity = await cartService.RemoveItem(cartId, itemId);
+        return cartEntity.ToResponse();
     }
 }
---
+++ OnlineShop/Cart.Api.Grpc/Services/Converters.cs
@@ -0,0 +1,45 @@
+﻿using Cart.Core.Entities;
+
+namespace Cart.Api.Services;
+
+public static class Converters
+{
+    public static ItemListResponse ToResponse(this CartEntity entity)
+    {
+        var response = new ItemListResponse();
+        response.Items.AddRange(entity.Items.Select(x => x.ToData()));
+        return response;
+    }
+
+    public static ItemData ToData(this CartItemEntity itemEntity)
+    {
+        var response = new ItemData
+        {
+            Id = itemEntity.Id,
+            ProductId = itemEntity.ProductId,
+            Name = itemEntity.Name,
+            Image = itemEntity.Image,
+            Price = (double)itemEntity.Price,
+            Quantity = itemEntity.Quantity
+        };
+        return response;
+    }
+
+    public static CartItemEntity ToItemEntity(this ItemData item)
+    {
+        var entity = new CartItemEntity();
+        entity.ProductId = item.ProductId;
+        entity.Name = item.Name;
+        entity.Image = item.Image;
+        entity.Price = (decimal)item.Price;
+        entity.Quantity = item.Quantity;
+        return entity;
+    }
+
+    public static ItemResponse ToResponse(this CartItemEntity itemEntity)
+    {
+        var response = new ItemResponse();
+        response.Data = itemEntity.ToData();
+        return response;
+    }
+}
